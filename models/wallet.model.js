dfc161674574a03a71ec0c44b222f9e3b62bf2c4
i50685837-sketch <i50685837@gmail.com>
2026-08-21 10:59:54 +0300

    Create wallet.model.js

---
+++ models/wallet.model.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const walletSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+      index: true
+    },
+
+    balance: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+
+    totalDeposited: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+
+    totalWithdrawn: {
+      type: Number,
+      default: 0,
+      min: 0
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Wallet", walletSchema);
